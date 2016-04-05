4b705ba61ffd5ce8b4662069f5eae108a9c43e22
madhavipuliraju <madhavipuliraju@gmail.com>
2016-04-05 18:28:27 +0530

    implemented the mail-service for feedback portal

--- app.py
+++ app.py
@@ -3,24 +3,15 @@ import urllib
 from flask import Flask, render_template, request, redirect, url_for, json,\
     flash
 import requests
-
+import smtplib
 import config
-from flask_mail import Mail, Message
+from email.mime.text import MIMEText
+#from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config.from_object(config)
-mail=Mail(app)
 
-app.config.update(
-    DEBUG=True,
-    #EMAIL SETTINGS
-    MAIL_HOST='smtp.admin.iiit.ac.in',
-    MAIL_SERVER='smtp.gmail.com',    
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME = 'madhavipuliraju@gmail.com',
-    MAIL_PASSWORD = '<password>'
-)
+
 @app.route('/', methods=['GET', 'POST'])
 def feedback_form():
     if request.method == 'GET':
@@ -81,9 +72,24 @@ def feedback_form():
                                  data=json.dumps(feedback_data))
 
         if response.status_code == 200:
-            msg = Message('Feedback-data from feedback-portal', sender = 'madhavipuliraju@gmail.com', recipients = ['madhavi@vlabs.ac.in', 'sripathi@vlabs.ac.in', 'kammari.sripathi@gmail.com'])
-            msg.body = json.dumps(feedback_data)
-            mail.send(msg)
+
+            smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+            smtpserver.ehlo()
+            smtpserver.starttls()
+            smtpserver.login('madhavipuliraju@gmail.com', 'thanishwar')
+            
+            msg = MIMEText(str(feedback_data["user_name"]))
+            msg['Subject'] = 'Data from feedback portal'
+            msg['From'] = "madhavipuliraju@gmail.com"
+            msg['To'] = "feedback@vlabs.ac.in"
+
+            # Send the message via our own SMTP server, but don't include the
+            # envelope header.
+
+            smtpserver.sendmail("madhavipuliraju@gmail.com", "feedback@vlabs.ac.in", msg.as_string())
+            smtpserver.quit()
+
+#            print feedback_data["user_name"]
             return redirect(url_for('thanks'))
         else:
             flash('Error posting your feedback')
