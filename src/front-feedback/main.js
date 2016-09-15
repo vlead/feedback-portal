a144f22b1233ee221ad0bac390c27030bf009893
Khushpreet <khushpreetk363@gmail.com>
2016-09-15 18:08:04 +0530

    Add bootstrap classes for style

--- src/front-feedback/main.js
+++ src/front-feedback/main.js
@@ -1,101 +1,117 @@
 var sampleData = {
-	"lab_name": "Data Structure",
-	"exp_name": "Array",
-	"user_id": "user123",
-	"data": "11",
-	"questions":
+	"lab_name": "Computer Prog Lab", 
+	"exp_name": "Array", 
+	"user_id": "user123", 
+	"data": "11", 
+	"questions": 
 		[
-			{
-			"name": "How this lab helped you understanding the concept?",
-			"type": "radioButton",
+			{ 
+			"name": "How did this experiment helped you understanding the concept?", 
+			"type": "radioButton", 
 			"options": ["fine", "good", "well", "not well"]
-			},
+			}, 
 
 			{
-			"name": "In which course you have this concept?",
+			"name": "Hobbies check?",
 			"type": "checkBox",
-			"options": ["B.tech", "M.Sc", "UG", "PG"]
+			"options": ["foo", "bar", "baz", "qux"]
 			},
 
 			{
-			"name": "Any Comments?",
-			"type": "textBox",
+			"name": "Any Comments?", 
+			"type": "textBox", 
 			}
 		]
 }
 
-var labelQues;
+var labelQues, formGroup;
 function renderQuesLabel() {
-	labelQues = document.createElement('label')
-	labelQues.className = "row col-md-12";
-	$("#questions_list").append(labelQues);
+	formGroup = document.createElement("div")
+	formGroup.className = "form-group"
+	labelQues = document.createElement( 'label')
+	labelQues.className = "col-sm-12"; 
+	document.getElementById("questions_list").appendChild(formGroup);
+	formGroup.appendChild(	labelQues)
+
 }
 
 function renderFeedbackform() {
-	$("#lab_name_label").html("Lab Name");
-	$("#lab_name_value").html(sampleData.lab_name);
-	$("#exp_name_label").html("Experiment Name");
-	$("#exp_name_value").html(sampleData.exp_name);
+	var lab_name_label = document.getElementById("lab_name_label");
+	lab_name_label.innerHTML = "Lab Name:";
+	var lab_name_value = document.getElementById("lab_name_value");
+	lab_name_value.value = sampleData.lab_name;
+
+	var exp_name_label = document.getElementById("exp_name_label");
+	exp_name_label.innerHTML= "Experiment Name:";
+	var exp_name_value = document.getElementById("exp_name_value");
+	exp_name_value.value = sampleData.exp_name;
 
 	var quesArray = sampleData.questions.length;
-	for (var i = 0; i < quesArray; i++)
+	for (var i = 0; i < quesArray; i++) 
 	{
-		if (sampleData.questions[i].type === "checkBox") {
+		if (sampleData.questions[i].type === "checkBox") { 
 			renderQuesLabel();
 			labelQues.innerHTML = sampleData.questions[i].name;
 			var optionsDiv = document.createElement("label");
-			optionsDiv.className = "col-md-4";
-      		optionsDiv.id = sampleData.questions[i].name;
+			optionsDiv.className = "col-sm-12";
+			optionsDiv.id = sampleData.questions[i].name;
 			var optionsLength = sampleData.questions[i].options.length;
 			for (var j = 0; j < optionsLength; j++) {
 				checkOption = document.createElement('input');
 				ansLabel = document.createElement('label');
 				checkOption.type = "checkbox";
-				checkOption.className = "col-md-1";
+				checkOption.className = "col-sm-1";
 				checkOption.value = sampleData.questions[i].options[j];
-				ansLabel.className = "col-md-2"
+				ansLabel.className = "col-sm-2"
 				ansLabel.innerHTML = sampleData.questions[i].options[j];
 				optionsDiv.appendChild(checkOption);
 				optionsDiv.appendChild(ansLabel);
-				$("#questions_list").append(optionsDiv);
+				formGroup.appendChild(optionsDiv);
+				questions_list.appendChild(formGroup);
+				// console.log("sampleData.questions[i].answers[j] is", sampleData.questions[i].answers[j])
 			}
-		}
+		}	
 		else if(sampleData.questions[i].type === "radioButton"){
 			renderQuesLabel();
 			labelQues.innerHTML = sampleData.questions[i].name;
 			var optionsDiv1 = document.createElement("div");
-      		optionsDiv1.id = sampleData.questions[i].name;
-			optionsDiv1.className = "col-md-4";
+			optionsDiv1.id = sampleData.questions[i].name;
+			optionsDiv1.className = "col-sm-12";
 			var optionsLength = sampleData.questions[i].options.length;
 			for (var k = 0; k < optionsLength; k++) {
 				radioOption = document.createElement('input');
 				radioLabel = document.createElement('label');
 				radioOption.type = "radio";
-				radioOption.className = "col-md-1";
+				radioOption.className = "col-sm-1";
 				radioLabel.className = "col-md-2";
 				radioOption.name = sampleData.questions[i].name;
 				radioOption.value = sampleData.questions[i].options[k];
 				radioLabel.innerHTML = sampleData.questions[i].options[k];
 				optionsDiv1.appendChild(radioOption);
 				optionsDiv1.appendChild(radioLabel);
-				$("#questions_list").append(optionsDiv1);
+				formGroup.appendChild(optionsDiv1);
+				questions_list.appendChild(formGroup);
+				console.log("radioblahhhhhhhhhhhhhhhhhhhhhhh");	
 			}
 		}
 
-		else if(sampleData.questions[i].type === 'textBox') {
+		else if(sampleData.questions[i].type === 'textBox'){
 			renderQuesLabel();
 			labelQues.innerHTML = sampleData.questions[i].name;
 			ansTextBox = document.createElement("input");
-			ansTextBox.className = "col-md-4"
-      		ansTextBox.id = sampleData.questions[i].name;
-			$("#questions_list").append(ansTextBox);
+			ansTextBox.className = "form-control col-md-6"
+			ansTextBox.id = sampleData.questions[i].name;
+			document.getElementById("questions_list").appendChild(ansTextBox);
+			console.log("text");
 		}
 	}
 }
 
 function submitFeedback() {
   	var feedback = {}; // the final feedback object
+
   	feedback.lab_name = sampleData.lab_name;
+  	feedback.exp_name = sampleData.exp_name;
   	feedback.questions = []; // the list of questions and user answers
 	var quesArray = sampleData.questions.length;
 
@@ -136,4 +152,5 @@ function submitFeedback() {
 	}
 	console.log(feedback); // the final object
 	console.log("Submitted feedback");
+	// console.log("Submitted feedback");
 }
